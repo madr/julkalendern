5ed6e5e8cecb8d411fb27c946a16f6b38e67dc5f
Anders Ytterström <anders.ytterstrom@iteam.se>
2021-11-04 08:52:41 +0100

    Solve day 5-6 2015

---
+++ 2015-elixir/lib/solutions/day_07.ex
@@ -0,0 +1,22 @@
+defmodule Aoc.Solution.Day07 do
+  @name "Day 7: Some Assembly Required"
+  @behaviour Solution
+
+  @impl Solution
+  def get_name, do: @name
+
+  @impl Solution
+  def parse!(_raw) do
+    "10"
+  end
+
+  @impl Solution
+  def solve_first_part(_input) do
+    "(TBW)"
+  end
+
+  @impl Solution
+  def solve_second_part(_input) do
+    "(TBW)"
+  end
+end
