90607ca0d4ecc8a7aca982e9686d24cbd3e536f7
Anders Englöf Ytterström <anders@playmaker.ai>
2023-12-15 11:13:06 +0100

    Add common helpers and constants

--- 2023-python/output/__init__.py
+++ 2023-python/output/__init__.py
@@ -1,7 +1,38 @@
 import functools
+import re
+
+# Directions/Adjacents for 2D matrices, in the order UP, RIGHT, DOWN, LEFT
+D = [
+    (-1, 0),
+    (0, 1),
+    (1, 0),
+    (0, -1),
+]
+
+# Directions for 2D matrices, as a dict with keys U, R, D, L
+DD = {
+    "U": (-1, 0),
+    "R": (0, 1),
+    "D": (1, 0),
+    "L": (0, -1),
+}
+
+# Adjacent relative positions including diagonals for 2D matrices, in the order NW, N, NE, W, E, SW, S, SE
+ADJ = [
+    (-1, -1),
+    (-1, 0),
+    (1, -1),
+    (0, -1),
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+]
 
 
 def answer(part_index, fmt_string):
+    """Decorator to present a solution in a human readable format"""
+
     def decorator_aoc(func):
         @functools.wraps(func)
         def wrapper_aoc(*args, **kwargs):
@@ -22,4 +53,29 @@ def answer(part_index, fmt_string):
 
 
 def headline(n, title):
+    """Print day number and name, followed by a ruler. Used by the answer decorator"""
     print(f"\n--- Day {n}: {title} ---\n")
+
+
+def ints(s):
+    """Extract all integers from a string"""
+    return [int(n) for n in re.findall(r"\d+", s)]
+
+
+def mhd(a, b):
+    """Calculates the Manhattan distance between 2 positions in the format (y, x) or (x, y)"""
+    ar, ac = a
+    br, bc = b
+    return abs(ar - br) + abs(ac - bc)
+
+
+def matrix(d):
+    """Transform a string into an iterable matrix. Returns the matrix, row count and col count"""
+    m = [tuple(r) for r in d.split()]
+    return m, len(m), len(m[0])
+
+
+def mdbg(m):
+    """Print-debug a matrix"""
+    for r in m:
+        print("".join(r))
